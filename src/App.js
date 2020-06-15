07d79a230c99318e4895bbb9a3ce51cf4bde7390
giammyisjammy <5416818+giammyisjammy@users.noreply.gitlab.com>
2020-06-15 16:29:58 +0200

    Minor fixes.

--- src/App.js
+++ src/App.js
@@ -1,90 +1,113 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import DataTable from "./dataTable";
-import { cleanData, getFingerprint, toSingleObject } from "./utils";
-import { Helmet } from "react-helmet";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import DataTable from './dataTable';
+import { cleanData, getFingerprint, toSingleObject } from './utils';
+import { Helmet } from 'react-helmet';
 
 function App() {
-  const [fingerprint, setFingerprint] = useState(null);
-  const [ipData, setIpData] = useState(null);
-  const [showReport, setShowReport] = useState(true);
+	const [fingerprint, setFingerprint] = useState(null);
+	const [ipData, setIpData] = useState(null);
+	const [hasFetchedData, setHasFetchedData] = useState(false);
+	const [errors, setErrors] = useState([]);
 
-  useEffect(() => {
-    if (showReport) {
-      fetch("https://extreme-ip-lookup.com/json")
-        .then(res => res.json(), console.error)
-        .then(ip => Promise.all([ip, getFingerprint()]))
-        .then(([ip, finger]) => {
-          const fp = finger
-            .map(({ key, value }) => ({ [key]: value }))
-            .reduce(toSingleObject);
-          const cleanFp = cleanData(fp);
-          const cleanIp = cleanData(ip);
+	useEffect(() => {
+		if (!hasFetchedData) {
+			const ipPromise = fetch(
+				'https://extreme-ip-lookup.com/json'
+			).then((res) => res.json());
+			const fingerPromise = getFingerprint();
+			Promise.all([ipPromise, fingerPromise])
+				.then(([ip, finger]) => {
+					const fp = finger
+						.map(({ key, value }) => ({ [key]: value }))
+						.reduce(toSingleObject);
+					const cleanFp = cleanData(fp);
+					const cleanIp = cleanData(ip);
 
-          setFingerprint(cleanFp);
-          setIpData(cleanIp);
-          setShowReport(false);
-        });
-    }
-  }, [showReport]);
+					setFingerprint(cleanFp);
+					setIpData(cleanIp);
+				})
+				.catch((error) => {
+					setErrors((prev) => [...prev, error.toString()]);
+				})
+				.finally(() => {
+					setHasFetchedData(true);
+				});
+		}
+	}, [hasFetchedData]);
 
-  return (
-    <div>
-      <Helmet>
-        <title>Seanky fingerprint and IP tracker</title>
-        <meta
-          name="description"
-          content="Small React app to collect a device's fingerprint and IP address metadata"
-        />
-        <meta
-          name="keywords"
-          cpntent="fingerprint,ip-address,tracker,react,privacy"
-        />
-      </Helmet>
-      <header>
-        <section>
-          <h1>
-            <a target='_blank' rel='noreferrer noopener' href='https://molamk.com/fingerprint'>
-              Sneaky Fingerprint And IP Address Tracker
-            </a>
-          </h1>
-          <p>
-            Questo è un progetto a solo scopo educativo. Nessun dato viene memorizzato. Giuro.
-          </p>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/giammyisjammy/fingerprint"
-          >
-            Codice su Github
-          </a>
-        </section>
-      </header>
-      {ipData && fingerprint ? (
-        <div>
-          <DataTable title="IP Data" data={ipData} />
-          <DataTable title="Fingerprint" data={fingerprint} />
-        </div>
-      ) : (
-          <section>
-            <h2>Please wait...</h2>
-          </section>
-        )}
-      <footer>
-        <p>
-          Made by{" "}
-          <a
-            style={{ fontWeight: 'bold' }}
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://molamk.com"
-          >
-            molamk
-          </a>
-        </p>
-      </footer>
-    </div>
-  );
+	return (
+		<div>
+			<Helmet>
+				<title>Seanky fingerprint and IP tracker</title>
+				<meta
+					name="description"
+					content="Small React app to collect a device's fingerprint and IP address metadata"
+				/>
+				<meta
+					name="keywords"
+					cpntent="fingerprint,ip-address,tracker,react,privacy"
+				/>
+			</Helmet>
+			<header>
+				<section>
+					<h1>
+						<a
+							target="_blank"
+							rel="noreferrer noopener"
+							href="https://github.com/giammyisjammy/fingerprint"
+						>
+							Sneaky Fingerprint And IP Address Tracker
+						</a>
+					</h1>
+					<p>
+						Questo è un progetto a solo scopo educativo. Nessun dato
+						viene memorizzato. Giuro.
+					</p>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://github.com/giammyisjammy/fingerprint"
+					>
+						Codice su Github
+					</a>
+				</section>
+			</header>
+			{hasFetchedData ? (
+				ipData && fingerprint ? (
+					<div>
+						<DataTable title="IP Data" data={ipData} />
+						<DataTable title="Fingerprint" data={fingerprint} />
+					</div>
+				) : (
+					<section>
+						{errors.map((err, key) => (
+							<h2 key={key} style={{ color: 'red' }}>
+								{err}
+							</h2>
+						))}
+					</section>
+				)
+			) : (
+				<section>
+					<h2>Please wait...</h2>
+				</section>
+			)}
+			<footer>
+				<p>
+					Made by{' '}
+					<a
+						style={{ fontWeight: 'bold' }}
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://github.com/giammyisjammy"
+					>
+						giammyisjammy
+					</a>
+				</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,24 @@
-import fp from "fingerprintjs2";
+import fp from 'fingerprintjs2';
 
-const notErrorOrUndefined = ([k, v]) => !(v === null || v === undefined || v instanceof Error);
-const arrayToString = ([k, v]) => Array.isArray(v) ? [k, v.join(', ')] : [k, v];
-const notEmptyString = ([k, v]) => !((typeof v === 'string' || v instanceof String) && v.lenght === 0);
-const boolToString = ([k, v]) => typeof v === 'boolean' ? [k, `${v}`] : [k, v];
+const notErrorOrUndefined = ([k, v]) =>
+	!(v === null || v === undefined || v instanceof Error);
+const arrayToString = ([k, v]) =>
+	Array.isArray(v) ? [k, v.join(', ')] : [k, v];
+const notEmptyString = ([k, v]) =>
+	!((typeof v === 'string' || v instanceof String) && v.lenght === 0);
+const boolToString = ([k, v]) =>
+	typeof v === 'boolean' ? [k, `${v}`] : [k, v];
 const flattenObject = ([k, v]) => ({ [k]: v });
 export const toSingleObject = (acc, curr) => ({ ...acc, ...curr });
 
-export const cleanData = data => {
-  return Object.entries(data)
-    .filter(notErrorOrUndefined)
-    .map(arrayToString)
-    .filter(notEmptyString)
-    .map(boolToString)
-    .map(flattenObject)
-    .reduce(toSingleObject)
+export const cleanData = (data) => {
+	return Object.entries(data)
+		.filter(notErrorOrUndefined)
+		.map(arrayToString)
+		.filter(notEmptyString)
+		.map(boolToString)
+		.map(flattenObject)
+		.reduce(toSingleObject);
 };
 
-export const getFingerprint = () =>
-  new Promise(resolve => {
-    fp.get(components => {
-      resolve(components);
-    });
-  });
+export const getFingerprint = () => fp.getPromise();
